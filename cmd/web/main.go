19d6db6eacd51ffb60bbf7edfd8e5b39f8f2816c
linus <linus@nasa.se>
2025-02-15 14:44:05 +0100

    adds global and doc paramters for the doc handler

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,18 +6,21 @@ import (
 
 	"github.com/gorilla/mux"
 
+	"github.com/xulinus/repogo/pkg/global"
 	"github.com/xulinus/repogo/pkg/handlers"
 )
 
 var port = "8085"
 
 func main() {
+	global.REPO = "xulinus/policy-docs"
+
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.PathPrefix("/css/").
 		Handler(http.StripPrefix("/css", handlers.NonListFileServer(http.FileServer(http.Dir("./tmpl/css/")))))
 
-	router.HandleFunc("/doc", handlers.Doc)
+	router.HandleFunc("/doc/{doc}", handlers.Doc)
 
 	log.Printf("Webserver listening on port %s", port)
 	http.ListenAndServe(":"+port, router)
