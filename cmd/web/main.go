cc25d79b074c120a7046b4a971cbd820603e15cf
linus <linus@nasa.se>
2025-02-15 11:13:36 +0100

    adds handler for documents

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/xulinus/repogo/pkg/handlers"
+)
+
+var port = "8085"
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/doc", handlers.Doc)
+
+	log.Printf("Webserver listening on port %s", port)
+	http.ListenAndServe(":"+port, router)
+}
